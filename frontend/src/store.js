0580feb0df40c804169139bff4de0a42e1f20a3d
AlexTech314 <alex.s@wustl.edu>
2020-11-12 10:49:52 -0600

    checkout wizard

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,9 +4,17 @@ import { cartReducer } from './reducers/cartReducers';
 import {
   productDetailsReducer,
   productListReducer,
-} from './reducers/productReducers';
+} from './reducers/productReducers';import {
+  userRegisterReducer,
+  userSigninReducer,
+} from './reducers/userReducers';
 
 const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -17,6 +25,8 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -24,5 +34,4 @@ const store = createStore(
   initialState,
   composeEnhancer(applyMiddleware(thunk))
 );
-
 export default store;
